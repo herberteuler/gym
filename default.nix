c3d066e6d88f39c5c16db1b1296a3e615a8e2461
Guanpeng Xu <herberteuler@gmail.com>
2018-06-30 13:44:33 +0800

    Add dependency to ffmpeg

--- default.nix
+++ default.nix
@@ -1,7 +1,9 @@
-{ stdenv, python34, python34Packages, ... }:
+{ stdenv, ffmpeg, python34, python34Packages, ... }:
 with python34Packages;
 buildPythonPackage rec {
   name = "gym";
   src = ./.;
-  buildInputs = [ numpy requests six pyglet tensorflow tensorflow-tensorboard ];
+  buildInputs = [
+    numpy requests six pyglet tensorflow tensorflow-tensorboard ffmpeg
+  ];
 }
