30d21a0f426f4d968bf6f9b5777a162db38a0112
Guanpeng Xu <herberteuler@gmail.com>
2018-06-30 13:42:03 +0800

    Add dependency to ffmpeg

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
-{ stdenv, python3, python3Packages, ... }:
+{ stdenv, ffmpeg, python3, python3Packages, ... }:
 with python3Packages;
 buildPythonPackage rec {
   name = "gym";
   src = ./.;
-  buildInputs = [ numpy requests six pyglet ];
+  buildInputs = [ numpy requests six pyglet ffmpeg ];
 }
